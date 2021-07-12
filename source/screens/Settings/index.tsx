c7e756c0f4ff43ff8bf59cc95de0ab81314c0e64
Luis A. Zapata Icart <expansion88@pm.me>
2021-07-12 17:36:04 -0300

    Initial Navigation Implemented

---
+++ source/screens/Settings/index.tsx
@@ -0,0 +1,19 @@
+/** @format */
+
+import React from 'react';
+import {
+  Text,
+  View,
+} from 'react-native';
+
+import { styles } from './styles';
+
+const Settings = () => {
+    return (
+      <View style={styles.text}>
+        <Text>Settings Screen</Text>
+      </View>
+    );
+  };
+
+export default Settings;

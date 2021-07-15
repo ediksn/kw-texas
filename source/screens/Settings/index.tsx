8a07d80d2c7832b4686c0a1459a76f88c41dc699
Ezequiel Leanes <ezequiel.leanes@gmail.com>
2021-07-15 16:51:30 -0300

    pod updated

--- source/screens/Settings/index.tsx
+++ source/screens/Settings/index.tsx
@@ -8,7 +8,7 @@ import { styles } from './styles';
 export const Settings = () => {
   return (
     <View style={styles.text}>
-      <Text>Settings Screen</Text>
+      <Text>Settings Screen Test</Text>
     </View>
   );
 };

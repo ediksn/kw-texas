eabdce085ab2dba8146dc17b63a6c0d63e5f1407
Luis A. Zapata Icart <expansion88@pm.me>
2021-07-19 09:10:34 -0300

    Comment fixed

--- source/screens/Settings/storybook/index.js
+++ source/screens/Settings/storybook/index.js
@@ -4,7 +4,7 @@ import { AppRegistry } from 'react-native';
 
 import { getStorybookUI, configure, addDecorator } from '@storybook/react-native';
 import { withKnobs } from '@storybook/addon-knobs';
-
+import { name as appName } from '../../../../app.json';
 import './rn-addons';
 import './stories';
 
@@ -14,6 +14,6 @@ configure(() => {}, module);
 
 const StorybookUIRoot = getStorybookUI({ asyncStorage: null });
 
-AppRegistry.registerComponent('%APP_NAME%', () => StorybookUIRoot);
+AppRegistry.registerComponent(appName, () => StorybookUIRoot);
 
 export default StorybookUIRoot;

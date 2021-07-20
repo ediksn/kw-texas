7d84fea9b09f5152459e0c5d28c6871a49741bd3
agendaenuelmorales <agenda.enuelmorales@gmail.com>
2021-07-20 11:56:44 -0300

    CNT-7803 add addon color and text to Story Book on Spinner Component

--- source/screens/Settings/storybook/stories/Spinner/Spinner.stories.tsx
+++ source/screens/Settings/storybook/stories/Spinner/Spinner.stories.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react-native'
+import { color, text } from '@storybook/addon-knobs'
 import Spinner from '~/components/Spinner'
 
-storiesOf('Spinner', module).add('Custom Spinner', () => <Spinner isLoading message='Loading' />)
+storiesOf('Spinner', module).add('Custom Spinner', () => (
+  <Spinner isLoading color={color('Background color')} message={text('Message text')} />
+))

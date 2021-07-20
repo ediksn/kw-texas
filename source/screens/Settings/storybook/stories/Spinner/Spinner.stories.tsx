629a6a3785394ff6035f8dbb38e1b19488ad9010
agendaenuelmorales <agenda.enuelmorales@gmail.com>
2021-07-20 12:49:39 -0300

    CNT-7803 add second parameter to addon knobs on color and text

--- source/screens/Settings/storybook/stories/Spinner/Spinner.stories.tsx
+++ source/screens/Settings/storybook/stories/Spinner/Spinner.stories.tsx
@@ -4,5 +4,5 @@ import { color, text } from '@storybook/addon-knobs'
 import Spinner from '~/components/Spinner'
 
 storiesOf('Spinner', module).add('Custom Spinner', () => (
-  <Spinner isLoading color={color('Background color')} message={text('Message text')} />
+  <Spinner isLoading color={color('Background color', '')} message={text('Message text', '')} />
 ))

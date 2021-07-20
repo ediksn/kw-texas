3fb895372468b3088ac93fc3d14bbae54afb07be
Ignacio Viejo <ignacionachoviejo@gmail.com>
2021-07-20 13:32:21 -0300

    feature/CNT-7802: Fix paths with ~

--- source/screens/Settings/storybook/index.ts
+++ source/screens/Settings/storybook/index.ts
@@ -5,7 +5,7 @@ import { AppRegistry } from 'react-native';
 import { getStorybookUI, configure, addDecorator } from '@storybook/react-native';
 import { withKnobs } from '@storybook/addon-knobs';
 
-import { name as appName } from '~/app.json';
+import { name as appName } from '../../../../app.json';
 import './rn-addons';
 import './stories';
 
--- source/screens/Settings/storybook/stories/Headers/Header.stories.tsx
+++ source/screens/Settings/storybook/stories/Headers/Header.stories.tsx
@@ -2,6 +2,6 @@
 
 import { storiesOf } from '@storybook/react-native';
 import React from 'react';
-import { Header } from '~/source/components/';
+import { Header } from '~/components/';
 
 storiesOf('Header', module).add('Only title', () => <Header title="Home" />);

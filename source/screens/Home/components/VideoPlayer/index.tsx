e3d4f54b79b6c30a2b7d646890283a8f42fb2f0b
Luis A. Zapata Icart <expansion88@pm.me>
2021-07-27 12:47:11 -0300

    Component implemented

---
+++ source/screens/Home/components/VideoPlayer/index.tsx
@@ -0,0 +1,88 @@
+import React, { useState } from 'react'
+import { Image, Text, View, TouchableOpacity } from 'react-native'
+import Video from 'react-native-video'
+
+import like_button from 'assets/images/like_btn.png'
+import like_buttonw from 'assets/images/like_btnw.png'
+import save_button from 'assets/images/save_btn.png'
+import save_buttonw from 'assets/images/save_btnw.png'
+import link_button from 'assets/images/link_btn.png'
+
+import { styles } from './styles'
+
+interface Props {
+  title: string
+  uri: string
+}
+
+const VideoPlayer = ({ title, uri }: Props) => {
+  const [likes, setlikes] = useState(876)
+  const [clicked, setClicked] = useState(false)
+  const [save, setSave] = useState(false)
+
+  const getClicked = () => {
+    if (clicked === false) {
+      setClicked(true)
+    } else {
+      setClicked(false)
+    }
+  }
+
+  const getLike = () => {
+    getClicked()
+    if (clicked) {
+      setlikes(likes - 1)
+    } else {
+      setlikes(likes + 1)
+    }
+  }
+
+  const getSaved = () => {
+    if (save) {
+      setSave(false)
+    } else {
+      setSave(true)
+    }
+  }
+
+  return (
+    <View style={styles.container}>
+      <Video source={{ uri }} fullscreen={false} controls repeat resizeMode='cover' style={styles.videoPlayer} />
+      <View style={styles.descContainer}>
+        <Text style={styles.text}>{title}</Text>
+        <View style={styles.btnView}>
+          <View style={styles.leftGroup}>
+            {clicked && (
+              <TouchableOpacity onPress={getLike}>
+                <Image style={styles.likeBtn} resizeMode='contain' source={like_button} />
+              </TouchableOpacity>
+            )}
+            {!clicked && (
+              <TouchableOpacity onPress={getLike}>
+                <Image style={styles.likeBtn} resizeMode='contain' source={like_buttonw} />
+              </TouchableOpacity>
+            )}
+            <Text style={styles.counter}>{likes}</Text>
+          </View>
+          <View style={styles.rightGroup}>
+            {save && (
+              <TouchableOpacity onPress={getSaved}>
+                <Image style={styles.saveBtn} resizeMode='contain' source={save_button} />
+              </TouchableOpacity>
+            )}
+            {!save && (
+              <TouchableOpacity onPress={getSaved}>
+                <Image style={styles.saveBtn} resizeMode='contain' source={save_buttonw} />
+              </TouchableOpacity>
+            )}
+            <TouchableOpacity>
+              <Image resizeMode='contain' source={link_button} />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    </View>
+  )
+}
+
+export default VideoPlayer
---
+++ source/screens/Home/components/VideoPlayer/styles.ts
@@ -0,0 +1,45 @@
+/** @format */
+
+import { StyleSheet } from 'react-native'
+import { theme } from '~/constants'
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  videoPlayer: {
+    width: '100%',
+    height: 300
+  },
+  descContainer: {
+    marginTop: 8,
+    backgroundColor: theme.backgroundWhite
+  },
+  text: {
+    textAlign: 'justify',
+    padding: 16,
+    fontFamily: 'Mulish-Bold',
+    lineHeight: 20
+  },
+  btnView: {
+    padding: 17.5,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  leftGroup: {
+    flexDirection: 'row'
+  },
+  likeBtn: {
+    marginRight: 9.5
+  },
+  counter: {
+    fontFamily: 'Mulish-Bold',
+    color: theme.darkGreenColor
+  },
+  rightGroup: {
+    flexDirection: 'row'
+  },
+  saveBtn: {
+    marginRight: 22
+  }
+})

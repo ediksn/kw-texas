7f3bbf19759c80441b9ba11f4e491d52d8bae468
Ignacio Viejo <ignacionachoviejo@gmail.com>
2021-07-28 12:30:24 -0300

    feature/CNT-7774: Style file and fix theme

--- source/constants/theme.ts
+++ source/constants/theme.ts
@@ -3,12 +3,13 @@
 export const theme = {
   backgroundDark: '#006C84',
   backgroundWhite: 'white',
-  activeColor: 'rgb(0,108,132)',
+  activeColor: '#006C84',
   inActiveColor: '#212121',
   placeholderTextColor: 'rgba(61, 66, 77, 1)',
   backgroundColor: '#FFFFFF',
   greenColor: '#6C727E',
   red: '#C5050B',
   darkGrey: '#282B33',
-  grey: 'rgb(200,200,200)'
+  grey: '#C8C8C8',
+  beige: '#fff8dc'
 }
--- source/screens/Settings/storybook/stories/VideoCard/VideoCard.stories.tsx
+++ source/screens/Settings/storybook/stories/VideoCard/VideoCard.stories.tsx
@@ -3,8 +3,10 @@
 import React, { createElement } from 'react'
 import { storiesOf } from '@storybook/react-native'
 import { color } from '@storybook/addon-knobs'
+import { moderateScale } from 'react-native-size-matters'
 import { VideoCard } from '~/components/'
 import cunhaVar from '~/assets/images/cunhavar.jpeg'
+import { theme } from '~/constants'
 
 storiesOf('Video Card', module).add('Video card', () =>
   createElement(() => {
@@ -15,9 +17,9 @@ storiesOf('Video Card', module).add('Video card', () =>
         author='LocoXRiverPlate1'
         visits={20.745}
         likes={912}
-        backgroundColor={color('Background color', '#fff8dc')}
-        textColor={color('Text color', '#ce011f')}
-        style={{ margin: '2%' }}
+        backgroundColor={color('Background color', theme.beige)}
+        textColor={color('Text color', theme.red)}
+        style={{ margin: moderateScale(2) }}
       />
     )
   })

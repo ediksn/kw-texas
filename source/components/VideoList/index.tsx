0cfcb1c4b83f4731a623dc892a5d1282a5d6f08c
Ignacio Viejo <ignacionachoviejo@gmail.com>
2021-07-28 18:48:03 -0300

    feature/CNT-7775: VideoList Component

--- source/components/VideoList/index.tsx
+++ source/components/VideoList/index.tsx
@@ -1,16 +1,18 @@
 import React from 'react'
-import { SafeAreaView } from 'react-native'
+import { ListRenderItem, SafeAreaView, ViewStyle } from 'react-native'
 import { FlatList } from 'react-native-gesture-handler'
 import { styles } from './styles'
 
 interface Props {
-  renderItem: any
-  data: []
+  renderItem: ListRenderItem<any>
+  data: any[]
+  keyExtractor: (item: any, index: number) => string
+  style?: ViewStyle
 }
-const VideoList = ({ renderItem, data }: Props) => {
+const VideoList = ({ renderItem, data, keyExtractor, style }: Props) => {
   return (
-    <SafeAreaView style={styles.container}>
-      <FlatList renderItem={renderItem} data={data} />
+    <SafeAreaView style={[styles.container, style]}>
+      <FlatList renderItem={renderItem} data={data} keyExtractor={keyExtractor} />
     </SafeAreaView>
   )
 }
--- source/screens/Settings/storybook/stories/VideoList/VideoList.stories.tsx
+++ source/screens/Settings/storybook/stories/VideoList/VideoList.stories.tsx
@@ -2,11 +2,64 @@
 
 import React, { createElement } from 'react'
 import { storiesOf } from '@storybook/react-native'
-import { View } from 'react-native'
-// import { VideoList } from '~/components/'
+import { color } from '@storybook/addon-knobs'
+import { moderateScale } from 'react-native-size-matters'
+import { VideoList, VideoCard } from '~/components/'
+import { theme } from '~/constants'
+import cunhaVar from '~/assets/images/cunhavar.jpeg'
+
+interface IVideo {
+  id: string
+  title: string
+  author: string
+  visits: number
+  likes: number
+}
 
 storiesOf('VideoList', module).add('VideoList', () =>
   createElement(() => {
-    return <View />
+    const videos: IVideo[] = [
+      {
+        id: '1',
+        title: 'Prueba 1',
+        author: 'Probador 1',
+        visits: 11111,
+        likes: 111
+      },
+      {
+        id: '2',
+        title: 'Prueba 2',
+        author: 'Probador 2',
+        visits: 22222,
+        likes: 222
+      },
+      {
+        id: '3',
+        title: 'Prueba 3',
+        author: 'Probador 3',
+        visits: 33333,
+        likes: 333
+      },
+      {
+        id: '4',
+        title: 'Prueba 4',
+        author: 'Probador 4',
+        visits: 44444,
+        likes: 444
+      }
+    ]
+    const renderVideoComponent = ({ item }: { item: IVideo }) => (
+      <VideoCard
+        img={cunhaVar}
+        title={item.title}
+        author={item.author}
+        visits={item.visits}
+        likes={item.likes}
+        backgroundColor={color('Background color', theme.beige)}
+        textColor={color('Text color', theme.red)}
+        style={{ margin: moderateScale(4) }}
+      />
+    )
+    return <VideoList renderItem={renderVideoComponent} data={videos} keyExtractor={(item: IVideo) => item.id} />
   })
 )

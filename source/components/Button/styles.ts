ad2bbb86cd9b97906fa003a39875bb3643098e9c
agendaenuelmorales <agenda.enuelmorales@gmail.com>
2021-07-29 15:19:58 -0300

    CNT-7773 add styles to component Button

--- source/components/Button/styles.ts
+++ source/components/Button/styles.ts
@@ -5,7 +5,8 @@ export const styles = StyleSheet.create({
   containerView: {
     height: verticalScale(30),
     width: scale(150),
-    elevation: 4,
+    elevation: 1,
+    borderWidth: 0,
     borderRadius: moderateScale(5),
     justifyContent: 'center',
     alignSelf: 'center',
@@ -19,3 +20,33 @@ export const styles = StyleSheet.create({
     fontWeight: 'bold'
   }
 })
+
+export const stylesOfType: any = {
+  CONTAINED: (theme: any) => ({
+    backgroundTypeStyle: {
+      backgroundColor: theme.backgroundColor
+    },
+    textTypeStyle: {
+      color: theme.color
+    }
+  }),
+  OUTLINED: (theme: any) => ({
+    backgroundTypeStyle: {
+      backgroundColor: 'transparent',
+      borderColor: theme.backgroundColor,
+      borderWidth: moderateScale(2)
+    },
+    textTypeStyle: {
+      color: theme.backgroundColor
+    }
+  }),
+  TEXT: (theme: any) => ({
+    backgroundTypeStyle: {
+      backgroundColor: 'transparent',
+      elevation: 0
+    },
+    textTypeStyle: {
+      color: theme.backgroundColor
+    }
+  })
+}

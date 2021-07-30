c35ed2f5ada7638196527c0db67fd3383e138962
Luis A. Zapata Icart <expansion88@pm.me>
2021-07-30 09:37:02 -0300

    Touchable area fixed

--- source/components/ListButton/index.tsx
+++ source/components/ListButton/index.tsx
@@ -18,19 +18,20 @@ const ListButton = ({ title, icon, onPress, arrow }: Props) => {
   const { t } = useTranslation()
 
   return (
-    <View style={styles.container}>
-      <View style={styles.textContainer}>
-        <TouchableHighlight onPress={onPress}>
+    <TouchableHighlight onPress={onPress}>
+      <View style={styles.container}>
+        <View style={styles.textContainer}>
           <Image style={styles.logoutBtn} resizeMode='contain' source={icon} />
-        </TouchableHighlight>
-        <Text style={styles.text}>{t(title)}</Text>
-      </View>
-      {arrow && (
-        <View>
-          <Image style={styles.arrowBtn} resizeMode='center' source={arrow_btn} />
+
+          <Text style={styles.text}>{t(title)}</Text>
         </View>
-      )}
-    </View>
+        {arrow && (
+          <View>
+            <Image style={styles.arrowBtn} resizeMode='center' source={arrow_btn} />
+          </View>
+        )}
+      </View>
+    </TouchableHighlight>
   )
 }
 

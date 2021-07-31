04e37f75da3f02f1c56a30778459a86736afbc70
Luis A. Zapata Icart <expansion88@pm.me>
2021-07-30 21:09:12 -0300

    New stack navigation added

--- App.tsx
+++ App.tsx
@@ -2,12 +2,25 @@ import React from 'react'
 import { SafeAreaView, LogBox } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { createStackNavigator } from '@react-navigation/stack'
 import setI18nConfig from '~/i18n'
 import { NAVIGATION } from '~/constants'
 import { Home, Settings } from '~/screens'
+import Storybook from '~/screens/Settings/storybook'
 
 // handy to turn off yellow box for testing purposes
 LogBox.ignoreAllLogs(false)
+
+const SettingsStack = createStackNavigator()
+
+const SettingsStackScreen = () => {
+  return (
+    <SettingsStack.Navigator>
+      <SettingsStack.Screen name='Settings' options={{ headerTitleAlign: 'center' }} component={Settings} />
+      <SettingsStack.Screen name='StoryBooks' component={Storybook} />
+    </SettingsStack.Navigator>
+  )
+}
 
 const Tab = createBottomTabNavigator()
 
@@ -17,7 +30,7 @@ const App = () => {
     <NavigationContainer>
       <Tab.Navigator initialRouteName='Home'>
         <Tab.Screen options={{ title: 'Home' }} name={NAVIGATION.SCREEN.HOME} component={Home} />
-        <Tab.Screen options={{ title: 'Settings' }} name={NAVIGATION.SCREEN.SETTINGS} component={Settings} />
+        <Tab.Screen options={{ title: 'Settings' }} name={NAVIGATION.SCREEN.SETTINGS} component={SettingsStackScreen} />
       </Tab.Navigator>
       <SafeAreaView />
     </NavigationContainer>

ab2649d03fc1dde458b4b3274955952422fd1cae
agendaenuelmorales <agenda.enuelmorales@gmail.com>
2021-08-01 21:25:38 -0300

    CNT-7786 login and main navigate routes on stack

--- source/screens/stackNavigation.tsx
+++ source/screens/stackNavigation.tsx
@@ -6,15 +6,11 @@ import { TabNavigator } from '~/components'
 
 const StackNavigation = () => {
   const RootStack = createStackNavigator()
-  const isLoggedIn = false
 
   return (
     <RootStack.Navigator headerMode='none'>
-      {isLoggedIn ? (
-        <RootStack.Screen name={NAVIGATION.SCREEN.MAIN} component={TabNavigator} />
-      ) : (
-        <RootStack.Screen name={NAVIGATION.SCREEN.LOGIN} component={Login} />
-      )}
+      <RootStack.Screen name={NAVIGATION.SCREEN.LOGIN} component={Login} />
+      <RootStack.Screen name={NAVIGATION.SCREEN.MAIN} component={TabNavigator} />
     </RootStack.Navigator>
   )
 }

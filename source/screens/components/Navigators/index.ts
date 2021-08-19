6e6465ca10550085c83e1dce59328c3239c668cd
Luis A. Zapata Icart <expansion88@pm.me>
2021-08-19 10:17:51 -0300

    Navigation Top Nav Bar implemented

--- source/screens/components/Navigators/index.ts
+++ source/screens/components/Navigators/index.ts
@@ -2,6 +2,12 @@
 
 import { createStackNavigator } from '@react-navigation/stack'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
+
+const TopTab = createMaterialTopTabNavigator()
+
+export const TopTabNavigator = TopTab.Navigator
+export const TopTabScreen = TopTab.Screen
 
 const Stack = createStackNavigator()
 

2428afc5f5a635a51e2e337d61b31bf5be7eed81
Ignacio Viejo <ignacionachoviejo@gmail.com>
2021-08-26 16:32:41 -0300

    feature/CNT-8112: Permissions for ios asked when is focused

--- source/screens/pages/Home/pages/Studio/index.tsx
+++ source/screens/pages/Home/pages/Studio/index.tsx
@@ -3,17 +3,15 @@
 import React from 'react'
 import { SafeAreaView } from 'react-native'
 import { useSelector } from 'react-redux'
+import { useIsFocused } from '@react-navigation/native'
 
 import { styles } from './styles'
 import Recording from '../../components/VideoCapture'
 import { RootState } from '~/store'
 
 export const Studio = () => {
+  const isFocused = useIsFocused()
   const usr: any = useSelector((state: RootState) => state.login)
 
-  return (
-    <SafeAreaView style={styles.container}>
-      <Recording usr={usr} />
-    </SafeAreaView>
-  )
+  return <SafeAreaView style={styles.container}>{isFocused && <Recording usr={usr} />}</SafeAreaView>
 }

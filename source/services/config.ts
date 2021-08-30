c95e655e1394da3754dadcdd90247d6445069d0a
Ignacio Viejo <ignacionachoviejo@gmail.com>
2021-08-30 12:37:19 -0300

    feature/CNT-8746: Added env setup

--- source/services/config.ts
+++ source/services/config.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
+import Config from 'react-native-config'
 import { StorageLogInResponse } from '~/interfaces/loginInterface'
 import { Storage, STORAGE_CONSTANTS } from '~/utils/storage'
-// import Config from 'react-native-config'
 
 const getToken = async () => {
   const { LOGIN } = STORAGE_CONSTANTS
@@ -10,12 +10,12 @@ const getToken = async () => {
 }
 
 export const axiosInstance = axios.create({
-  baseURL: 'https://qa-kong.command-api.kw.com/', // Config.baseURL,
+  baseURL: Config.baseURL,
   headers: { 'Content-Type': 'application/json' }
 })
 
 export const axiosInstanceTokens = async () =>
   axios.create({
-    baseURL: 'https://qa-kong.command-api.kw.com/', // Config.baseURL,
+    baseURL: Config.baseURL,
     headers: { 'Content-Type': 'application/json', 'Authorization': await getToken() }
   })

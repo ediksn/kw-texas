4764371150f188da46f8b3304defb081c3b95637
Luis A. Zapata Icart <expansion88@pm.me>
2021-09-06 13:09:01 -0300

    Profile button implemented (CNT-8227)

--- source/screens/pages/Settings/index.tsx
+++ source/screens/pages/Settings/index.tsx
@@ -4,6 +4,7 @@ import React from 'react'
 import { SafeAreaView } from 'react-native'
 
 import logout_btn from 'assets/images/logout_btn.png'
+import profile_btn from 'assets/images/profile_btn.png'
 import { useDispatch } from 'react-redux'
 import { styles } from './styles'
 import ListButton from '~/components/ListButton'
@@ -27,7 +28,8 @@ export const Settings = ({ navigation }: Props) => {
         onPress={() => navigation.navigate(NAVIGATION.SCREEN.STORYBOOK)}
         arrow
       />
-      <ListButton title='Log Out' icon={logout_btn} onPress={handleLogOut} arrow />
+      <ListButton title='Profile' icon={logout_btn} onPress={() => null} arrow />
+      <ListButton title='Log Out' icon={profile_btn} onPress={handleLogOut} arrow />
     </SafeAreaView>
   )
 }

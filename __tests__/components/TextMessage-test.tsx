ceb8534056e23b19cf6626c5376e028f2adeef46
Ignacio Viejo <ignacionachoviejo@gmail.com>
2021-09-08 17:04:05 -0300

    changed name to message

--- __tests__/components/TextMessage-test.tsx
+++ __tests__/components/TextMessage-test.tsx
@@ -3,6 +3,6 @@ import renderer from 'react-test-renderer'
 import TextMessage from '~/components/TextMessage'
 
 test('renders correctly', () => {
-  const header = renderer.create(<TextMessage type='error' message='This is a example message' />).toJSON()
-  expect(header).toMatchSnapshot()
+  const message = renderer.create(<TextMessage type='error' message='This is a example message' />).toJSON()
+  expect(message).toMatchSnapshot()
 })

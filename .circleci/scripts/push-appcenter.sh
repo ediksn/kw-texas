1fc7674d5ce3c7011624e1155c8d34ada4f0c5c3
agendaenuelmorales <agenda.enuelmorales@gmail.com>
2021-09-27 17:34:33 -0300

    CNT-9006 add push appcenter to circle ci config FIX 8

--- .circleci/scripts/push-appcenter.sh
+++ .circleci/scripts/push-appcenter.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-cd ios && bundle install --path bundle
+cd ios && bundle config set path 'vendor/bundle' && bundle install --path bundle
 # bundle exec fastlane ios push
 bundle exec fastlane android push

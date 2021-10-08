14bb2f766a5f13e75a25d97aecff3b476ad4126c
Ignacio Viejo <ignacionachoviejo@gmail.com>
2021-10-08 10:54:13 -0300

    back button handler for exit app

--- source/screens/pages/Home/index.tsx
+++ source/screens/pages/Home/index.tsx
@@ -1,16 +1,20 @@
 import React, { useEffect } from 'react'
 import { SafeAreaView, View } from 'react-native'
 import { useDispatch, useSelector } from 'react-redux'
+
 import { styles } from './styles'
 import Post from '~/components/Post'
 import { RootState } from '~/store'
 import { getUsrProfileActions } from '~/store/actions'
+import { useBackButtonMinimize } from '~/hooks'
 
 export const Home = () => {
   const dispatch = useDispatch()
   const usr: any = useSelector((state: RootState) => state.login.user)
   const usrProfile: any = useSelector((state: RootState) => state.usrProfile.profiles)
   const usrId: number = usr?.kwuid
+
+  useBackButtonMinimize()
 
   useEffect(() => {
     if (usrProfile.length === 0) dispatch(getUsrProfileActions.getUsrProfile(usrId))
--- source/screens/pages/Notifications/index.tsx
+++ source/screens/pages/Notifications/index.tsx
@@ -2,8 +2,10 @@
 
 import React from 'react'
 import { SafeAreaView, Text } from 'react-native'
+import { useBackButtonMinimize } from '~/hooks'
 
 export const Notifications = () => {
+  useBackButtonMinimize()
   return (
     <SafeAreaView>
       <Text>Notifications</Text>

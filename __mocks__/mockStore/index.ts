0a509ab127026de00b3cde67b38c683ede098e41
agendaenuelmorales <agenda.enuelmorales@gmail.com>
2021-10-13 09:22:57 -0300

    CNT-9674 add state redux test

--- __mocks__/mockStore/index.ts
+++ __mocks__/mockStore/index.ts
@@ -1,13 +1,16 @@
 import configureMockStore from 'redux-mock-store'
-import thunk from 'redux-thunk'
+import { AnyAction } from 'redux'
+import thunk, { ThunkDispatch } from 'redux-thunk'
 import { homeModel, loginModel, promptVideoModel } from '~/store/model'
 import { VIDEO_INITIAL_STATE } from '~/store/model/videoModel'
 import { VIDEO_INITIAL_STATE as BM_VIDEO_INITIAL_STATE } from '~/store/model/bmModel'
 import { UPLOAD_INITIAL_STATE } from '~/store/model/uploadVideoModel'
 import { USRPROFILE_INITIAL_STATE } from '~/store/model/usrProfileModel'
+import { RootState } from '~/store'
 
+type DispatchExts = ThunkDispatch<RootState, void, AnyAction>
 const middlewares = [thunk]
-const mockStore = configureMockStore(middlewares)
+const mockStore = configureMockStore<RootState, DispatchExts>(middlewares)
 const store = mockStore({
   login: loginModel,
   videos: VIDEO_INITIAL_STATE,
--- __tests__/screens/home/index-test.tsx
+++ __tests__/screens/home/index-test.tsx
@@ -1,9 +1,7 @@
-import { useSelector } from 'react-redux'
-import { act } from '@testing-library/react-native'
+// import axios from 'axios'
 import { postsResponse } from '../../../__mocks__/mockResponses'
 import store from '../../../__mocks__/mockStore'
-import { homeActions } from '~/store/actions'
-import { RootState } from '~/store'
+// import { homeActions } from '~/store/actions'
 import { HOME_TYPES } from '~/store/types'
 import { homeReducer } from '~/store/reducers'
 import { ProduceProps } from '~/interfaces/reducerInterface'
@@ -31,8 +29,11 @@ jest.mock(
   { virtual: true }
 )
 
+jest.mock('axios')
+
 const LIMIT = 10
 const { GET_POSTS, GET_POSTS_SUCCESS } = HOME_TYPES
+
 const expectedActions: ProduceProps[] = [
   {
     type: GET_POSTS
@@ -47,19 +48,30 @@ const expectedActions: ProduceProps[] = [
   }
 ]
 
-it('should create an action GET_POSTS_SUCCESS when user go to Home', async () => {
-  // const promise = Promise.resolve(homeActions.getPosts(LIMIT))
-  // await act(() => promise)
-  store.dispatch(homeActions.getPosts(LIMIT))
-  expect(store.getActions()).toEqual(expectedActions)
+describe('Home', () => {
+  // ${Config.BASE_URL}
+  // const mockedAxios = axios as jest.Mocked<typeof axios>
+  // mockedAxios.create({
+  //   baseURL: 'https://qa-kong.command-api.kw.com',
+  //   timeout: 5000,
+  //   headers: { 'Content-Type': 'application/json', 'Authorization': '' }
+  // })
+  // // mockedAxios.get.mockResolvedValue({ data: postsResponse })
+  // mockedAxios.get.mockImplementationOnce(() => Promise.resolve(postsResponse))
+  // mockedAxios.get(`/connect/posts?page=1&limit=${LIMIT}&pinned_at_top=false`, {})
+
+  // it('should create an action GET_POSTS_SUCCESS when user go to Home', async () => {
+  //   store.dispatch(homeActions.getPosts(LIMIT))
+  //   expect(store.getActions()).toEqual(expectedActions)
+  // })
+
+  it('should return array of posts in state', () => {
+    const oldStore: any = store.getState()
+    const expectedStore = { ...oldStore }
+    const mockAction: any = expectedActions.find(action => action.type === GET_POSTS_SUCCESS)
+    expectedStore.home.posts = { ...oldStore.home.posts, ...mockAction?.payload }
+    expectedStore.home.posts.isLoading = false
+
+    expect(homeReducer(oldStore.home, mockAction)).toEqual(expectedStore.home)
+  })
 })
-
-// it('should return array of posts in state', () => {
-//   const oldStore: any = store.getState()
-//   const expectedStore = { ...oldStore }
-//   const mockAction: any = expectedActions.find(action => action.type === GET_POSTS_SUCCESS)
-//   expectedStore.home.posts = { ...oldStore.home.posts, ...mockAction?.payload }
-//   expectedStore.home.posts.isLoading = false
-
-//   expect(homeReducer(oldStore, mockAction)).toEqual(expectedStore)
-// })

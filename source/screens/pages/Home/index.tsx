d0a7c934cccd7a0397e50fdcbb9db755d61b3f64
Mackewinsson <mackewinsson@gmail.com>
2021-10-28 10:09:46 -0300

    Empty post list updated (#136)
    Co-authored-by: Mackewinsson P <mackewinssonp.@MacBook-Pro-de-Mackewinsson.local>

--- source/screens/pages/Home/index.tsx
+++ source/screens/pages/Home/index.tsx
@@ -1,13 +1,15 @@
 import React, { useEffect } from 'react'
-import { SafeAreaView, View, FlatList } from 'react-native'
+import { useTranslation } from 'react-i18next'
+import { View, FlatList, Text } from 'react-native'
 import { useDispatch, useSelector } from 'react-redux'
-
+import { theme } from '~/constants'
 import { styles } from './styles'
 import Post from '~/components/Post'
 import { PostInterface } from '~/interfaces/postInterface'
 import { RootState } from '~/store'
 import { getUsrProfileActions, homeActions } from '~/store/actions'
 import { useBackButtonMinimize } from '~/hooks'
+import { Button, Icon } from '~/components'
 
 export const Home = () => {
   const dispatch = useDispatch()
@@ -18,8 +20,8 @@ export const Home = () => {
   const limit: number = useSelector((state: RootState) => state.home.posts.limit)
   const loading: boolean = useSelector((state: RootState) => state.home.posts.isLoading)
   const usrId: number = usr?.kwuid
-
   useBackButtonMinimize()
+  const { t } = useTranslation()
 
   useEffect(() => {
     if (usrProfile.length === 0) dispatch(getUsrProfileActions.getUsrProfile(usrId))
@@ -31,19 +33,37 @@ export const Home = () => {
   const onRefresh = () => dispatch(homeActions.getPosts(limit))
   const onEndReached = () => dispatch(homeActions.getPosts(limit + 10))
 
+  const NoPost = () => (
+    <View style={styles.noPostWrapper}>
+      <Icon name='message-icon' size={55} color={theme.backgroundDark} />
+      <Text style={styles.noPostMessage} testID='no_post_message'>
+        {t('No post message')}
+      </Text>
+      <Text style={styles.noPostMessage2} testID='no_post_message2'>
+        {t('No post message2')}
+      </Text>
+      <Button
+        message={t('Create a new post')}
+        viewStyle={styles.button}
+        textStyle={styles.buttonText}
+        testID='create_new_post_button'
+      />
+    </View>
+  )
+
   return (
-    <SafeAreaView>
-      <View style={styles.container}>
-        <FlatList
-          renderItem={renderPost}
-          data={posts}
-          keyExtractor={keyExtractor}
-          refreshing={loading}
-          onRefresh={onRefresh}
-          onEndReached={onEndReached}
-          onEndReachedThreshold={1}
-        />
-      </View>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <FlatList
+        renderItem={renderPost}
+        data={posts}
+        keyExtractor={keyExtractor}
+        refreshing={loading}
+        onRefresh={onRefresh}
+        onEndReached={onEndReached}
+        onEndReachedThreshold={1}
+        ListEmptyComponent={NoPost}
+        contentContainerStyle={styles.constentListStyle}
+      />
+    </View>
   )
 }
--- source/screens/pages/Home/styles.ts
+++ source/screens/pages/Home/styles.ts
@@ -1,9 +1,47 @@
 import { StyleSheet } from 'react-native'
-import { moderateScale } from 'react-native-size-matters'
+import { scale, verticalScale, moderateScale } from 'react-native-size-matters'
 
 export const styles = StyleSheet.create({
   container: {
-    paddingHorizontal: moderateScale(10),
-    paddingTop: moderateScale(10)
+    padding: moderateScale(10),
+    flex: 1
+  },
+  constentListStyle: {
+    flex: 1
+  },
+  noPostWrapper: {
+    justifyContent: 'center',
+    alignSelf: 'center',
+    alignItems: 'center',
+    position: 'relative',
+    paddingBottom: verticalScale(50),
+    flex: 1
+  },
+  noPostMessage: {
+    marginTop: verticalScale(36),
+    fontWeight: 'bold',
+    textAlign: 'center',
+    fontSize: 16,
+    fontFamily: 'Mulish-Bold'
+  },
+  noPostMessage2: {
+    marginTop: verticalScale(16),
+    textAlign: 'center',
+    paddingLeft: scale(40),
+    paddingRight: scale(40),
+    fontSize: 12,
+    fontFamily: 'Mulish-Bold'
+  },
+  button: {
+    marginTop: verticalScale(100),
+    position: 'absolute',
+    bottom: 0,
+    height: 48,
+    width: '100%'
+  },
+  buttonText: {
+    fontSize: 14,
+    color: '#fff',
+    fontFamily: 'Mulish-Bold'
   }
 })
